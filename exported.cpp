94de6487dee9e77722557c94fd48ae4c66943720
Chip Diamond <63815341+MrChipDiamond@users.noreply.github.com>
2024-08-13 20:08:05 -0400

    Requires Updating. Cheese is external.

---
+++ exported.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "exported.h"
+#if __has_include("exported.g.cpp")
+#include "exported.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::fnsln::implementation
+{
+    int32_t exported::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void exported::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void exported::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        Button().Content(box_value(L"Clicked"));
+    }
+}
---
+++ exported.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "exported.g.h"
+
+namespace winrt::fnsln::implementation
+{
+    struct exported : exportedT<exported>
+    {
+        exported() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::fnsln::factory_implementation
+{
+    struct exported : exportedT<exported, implementation::exported>
+    {
+    };
+}
